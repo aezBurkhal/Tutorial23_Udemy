f76ef84769e13e4d54d8d9d2dea9e2db5ca46375
aezBurkhal <44988851+aezBurkhal@users.noreply.github.com>
2018-12-12 11:37:16 -0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,21 @@
+
+public class Car extends Machine {
+
+	
+		
+		@Override
+	public void start() {
+		System.out.println("Car Started");
+	}
+
+		public void wipeWindShield() {
+			System.out.println("Wiping windshield.");
+
+
+	}
+		
+		public void showInfo() {
+			System.out.println("Car name: " + name);
+		}
+
+}
---
+++ Machine.java
@@ -0,0 +1,12 @@
+
+public class Machine {
+
+	protected String name = "Machine type one";
+	public void start() {
+		System.out.println("Machine started.");
+
+	}
+public void stop() {
+	System.out.println("Machine stopped.");
+}
+}
